015c7314113706eb79637e2cf3599cc1995a23dc
Pranto <prantroy23006@gmail.com>
2024-07-31 02:03:12 +0600

    - fit: change ajker bajar dor card name and image

--- API/GetTodaysMarketPrice.php
+++ API/GetTodaysMarketPrice.php
@@ -5,36 +5,42 @@
 <!-- Entry Button Section -->
 <section id="categories" class="py-5">
             <div class="container-fluid">
-                <h2 class="text-center section-title mb-4">বাজার ডিরেক্টরি</h2>
+                <h2 class="text-center section-title mb-4"> আজকের বাজার দর </h2>
                 <div class="row justify-content-center">
                     <div class="col-2 col-responsive" onclick="GetTodaysMarketPrice()">
                         <div class="feature-card">
-                            <img src="assets/images/cart/cart1.png" alt="Feature 1" class="card-img">
+                            <img src="assets/images/cart/rice.png" alt="Feature 1" class="card-img">
                             <h4 class="mt-2">  ধান </h4>
                         </div>
                     </div>
                     <div class="col-2 col-responsive">
                         <div class="feature-card">
-                            <img src="assets/images/cart/cart2.png" alt="Feature 2" class="card-img">
-                            <h4 class="mt-2">সপ্তাহের বাজার দর</h4>
+                            <img src="assets/images/cart/vurta.png" alt="Feature 2" class="card-img">
+                            <h4 class="mt-2">ভূট্টা </h4>
                         </div>
                     </div>
                     <div class="col-2 col-responsive">
                         <div class="feature-card">
-                            <img src="assets/images/cart/cart3.png" alt="Feature 3" class="card-img">
-                            <h4 class="mt-2">সর্বোচ্চ ও সর্বনিম্ন দরের আঞ্চলিক গ্রাফ</h4>
+                            <img src="assets/images/cart/peyaj.png" alt="Feature 3" class="card-img">
+                            <h4 class="mt-2">পেঁয়াজ  </h4>
                         </div>
                     </div>
                     <div class="col-2 col-responsive">
                         <div class="feature-card">
-                            <img src="assets/images/cart/cart4.png" alt="Feature 4" class="card-img">
-                            <h4 class="mt-2">কৃষক ও ব্যবসায়ী প্রফিট লিস্ট</h4>
+                            <img src="assets/images/cart/rosun.png" alt="Feature 4" class="card-img">
+                            <h4 class="mt-2">রসুন </h4>
                         </div>
                     </div>
                     <div class="col-2 col-responsive">
                         <div class="feature-card">
-                            <img src="assets/images/cart/cart5.png" alt="Feature 5" class="card-img">
-                            <h4 class="mt-2">ক্রয় বিক্রয়</h4>
+                            <img src="assets/images/cart/alu.png" alt="Feature 5" class="card-img">
+                            <h4 class="mt-2">আলু </h4>
+                        </div>
+                    </div>
+                    <div class="col-2 col-responsive">
+                        <div class="feature-card">
+                            <img src="assets/images/cart/ada.png" alt="Feature 5" class="card-img">
+                            <h4 class="mt-2">আদা </h4>
                         </div>
                     </div>
                 </div>
